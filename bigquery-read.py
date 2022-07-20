ad20d9977375647e112e3694ee8bfe7f89d124c8
ashah661 <105964553+ashah661@users.noreply.github.com>
2022-07-20 16:35:48 +0530

    Update and rename big query-read.py to bigquery-read.py

---
+++ bigquery-read.py
@@ -0,0 +1,19 @@
+#install the google cloud bigquery client services api
+pip install google-cloud-bigquery
+
+import os
+import pandas as pd
+
+from google.cloud import bigquery
+
+#create a service account and create a key for the service account on Cloud Console. Download the key (json format) on your machine
+credential_path = "<your_path>/<your_key>.jon"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
+
+client = bigquery.Client()
+
+query = """
+    SELECT * FROM `<YOUR_PROJECT_ID>.<DATASET>.<TABLE>`
+"""
+query_job = client.query(query)
+df = query_job.to_dataframe()
