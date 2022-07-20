ab4035d6b8b2cc069d1cee3e547d8251f40abbe5
ashah661 <105964553+ashah661@users.noreply.github.com>
2022-07-20 16:36:42 +0530

    Update bigquery-read.py

--- bigquery-read.py
+++ bigquery-read.py
@@ -6,7 +6,10 @@ import pandas as pd
 
 from google.cloud import bigquery
 
-#create a service account and create a key for the service account on Cloud Console. Download the key (json format) on your machine
+#create a service account for the associated PROJECT_ID
+#create a key for the service account on Cloud Console
+#Download the key (json format) on your machine
+
 credential_path = "<your_path>/<your_key>.jon"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
 
